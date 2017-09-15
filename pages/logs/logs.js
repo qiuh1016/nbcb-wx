a9d06929b91f230518e90b07ec8e483f4184dab3
qiuh1016 <qiuh1016@126.com>
2017-09-15 08:16:22 +0800

    test own map

--- pages/logs/logs.js
+++ pages/logs/logs.js
@@ -2,7 +2,30 @@
 var util = require('../../utils/util.js')
 Page({
   data: {
-    logs: []
+    logs: [],
+    events:[
+      {
+        id:1,
+        title: "123",
+        date:"2017/07/25 14:50:21",
+        finished: true
+      },
+      {
+        id: 2,
+        title: "1234",
+        date: "2017/07/25 14:50:21",
+        finished: true
+      },
+      {
+        id: 3,
+        title: "1235",
+        date: "2017/07/25 14:50:21",
+        finished: true
+      }
+    ]
+  },
+  checkboxChange: function(e) {
+    console.log(e.detail.value)
   },
   onLoad: function () {
     this.setData({
